53217363f615176c854d8e1e4a8276542dbf2718
Jakub Walczak <jwalczak33@gmail.com>
2021-01-28 18:19:31 +0100

    App title changed, initial results deleted

--- flappy-fish-api/flappy/schema.sql
+++ flappy-fish-api/flappy/schema.sql
@@ -3,9 +3,3 @@ CREATE TABLE IF NOT EXISTS results (
     score INTEGER,
     result_date TEXT
 );
-
-INSERT INTO results(username, score, result_date) VALUES('Champion1', 2, '2021-01-20T15:00:00.000Z');
-INSERT INTO results(username, score, result_date) VALUES('Champion1', 4, '2021-01-22T15:00:00.000Z');
-INSERT INTO results(username, score, result_date) VALUES('Champion2', 4, '2021-01-22T15:01:00.000Z');
-INSERT INTO results(username, score, result_date) VALUES('Champion2', 4, '2021-01-24T15:00:00.000Z');
-INSERT INTO results(username, score, result_date) VALUES('Champion2', 5, '2021-01-24T15:00:00.000Z');
